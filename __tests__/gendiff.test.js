50bcda2dadb6dfbb93b45ce5f06df67c44082fe7
ekaterina iakovleva <katyakov777@mail.ru>
2021-03-29 22:07:32 +0200

    modify test for nested structures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,7 +2,8 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import * as fs from 'fs';
 import * as path from 'path';
-import genDiff from '../src/gendiff-logic';
+import showChanges from '../src/index.js';
+import parsing from '../src/parsing.js';
 
 // eslint-disable-next-line no-underscore-dangle
 const __filename = fileURLToPath(import.meta.url);
@@ -11,15 +12,19 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 const fileWithResultName = 'expected_file.txt';
+let object1 = parsing(getFixturePath('test3.json'));
+let object2 = parsing(getFixturePath('test4.json'));
 
 // eslint-disable-next-line no-undef
 test('genDiff-json', () => {
   // eslint-disable-next-line no-undef
-  expect(genDiff(getFixturePath('test1.json'), getFixturePath('test2.json'))).toBe(readFile(fileWithResultName));
+  expect(showChanges(object1, object2)).toBe(readFile(fileWithResultName));
 });
 
 // eslint-disable-next-line no-undef
 test('genDiff-yaml', () => {
+  object1 = parsing(getFixturePath('test5.yml'));
+  object2 = parsing(getFixturePath('test6.yml'));
   // eslint-disable-next-line no-undef
-  expect(genDiff(getFixturePath('test3.yml'), getFixturePath('test4.yml'))).toBe(readFile(fileWithResultName));
+  expect(showChanges(object1, object2)).toBe(readFile(fileWithResultName));
 });

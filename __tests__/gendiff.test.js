25958fd9f6773d23a1fc6b1ca6cf50fe1f0ce541
ekaterina iakovleva <katyakov777@mail.ru>
2021-03-30 19:42:29 +0200

    add new tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,8 +2,16 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import * as fs from 'fs';
 import * as path from 'path';
-import showChanges from '../src/index.js';
-import parsing from '../src/parsing.js';
+import genDiff from '../src/gendiff-logic.js';
+
+const isJsonParsable = (string) => {
+  try {
+    JSON.parse(string);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
 
 // eslint-disable-next-line no-underscore-dangle
 const __filename = fileURLToPath(import.meta.url);
@@ -11,20 +19,30 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const fileWithResultName = 'expected_file.txt';
-let object1 = parsing(getFixturePath('test3.json'));
-let object2 = parsing(getFixturePath('test4.json'));
+// const fileWithResultName = 'expected_file.txt';
+const fileWithResultForStylish = 'stylish_output.txt';
+const fileWithResultForPlain = 'plain_output.txt';
 
 // eslint-disable-next-line no-undef
 test('genDiff-json', () => {
+  const path1 = getFixturePath('json_before.json');
+  const path2 = getFixturePath('json_after.json');
   // eslint-disable-next-line no-undef
-  expect(showChanges(object1, object2)).toBe(readFile(fileWithResultName));
+  expect(genDiff(path1, path2, 'stylish')).toBe(readFile(fileWithResultForStylish));
+  // eslint-disable-next-line no-undef
+  expect(genDiff(path1, path2, 'plain')).toBe(readFile(fileWithResultForPlain));
+  // eslint-disable-next-line no-undef
+  expect(isJsonParsable(genDiff(path1, path2, 'json'))).toBe(true);
 });
 
 // eslint-disable-next-line no-undef
 test('genDiff-yaml', () => {
-  object1 = parsing(getFixturePath('test5.yml'));
-  object2 = parsing(getFixturePath('test6.yml'));
+  const path1Yaml = getFixturePath('yaml_before.yml');
+  const path2Yaml = getFixturePath('yaml_after.yml');
   // eslint-disable-next-line no-undef
-  expect(showChanges(object1, object2)).toBe(readFile(fileWithResultName));
+  expect(genDiff(path1Yaml, path2Yaml, 'stylish')).toBe(readFile(fileWithResultForStylish));
+  // eslint-disable-next-line no-undef
+  expect(genDiff(path1Yaml, path2Yaml, 'plain')).toBe(readFile(fileWithResultForPlain));
+  // eslint-disable-next-line no-undef
+  expect(isJsonParsable(genDiff(path1Yaml, path2Yaml, 'json'))).toBe(true);
 });
